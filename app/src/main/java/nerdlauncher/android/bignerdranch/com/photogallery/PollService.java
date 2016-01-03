2aedc423783081527552f3a3645c522e0708b9d4
slad08 <slad_den@mail.ru>
2016-01-03 23:51:12 +0300

    Service pg.477

---
+++ app/src/main/java/nerdlauncher/android/bignerdranch/com/photogallery/PollService.java
@@ -0,0 +1,29 @@
+package nerdlauncher.android.bignerdranch.com.photogallery;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+/**
+ * Created by Denis on 03.01.2016.
+ */
+public class PollService extends IntentService {
+    private static final String TAG = "PollService";
+    public PollService() {
+        super(TAG);
+    }
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        ConnectivityManager cm=(ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        @SuppressWarnings("deprecation")
+                boolean isNetworkAvailable=cm.getBackgroundDataSetting()&&
+                cm.getActiveNetworkInfo()!=null;
+        if (!isNetworkAvailable)return;
+
+        Log.i(TAG, "Received an intent: " + intent);
+    }
+
+}
