aaeb42a600daa769b594e2a9eec16415bcdf353e
slad08 <slad_den@mail.ru>
2016-01-05 00:30:51 +0300

    Notification pg 487

--- app/src/main/java/nerdlauncher/android/bignerdranch/com/photogallery/PollService.java
+++ app/src/main/java/nerdlauncher/android/bignerdranch/com/photogallery/PollService.java
@@ -2,12 +2,16 @@ package nerdlauncher.android.bignerdranch.com.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -24,36 +28,51 @@ public class PollService extends IntentService {
     }
     @Override
     protected void onHandleIntent(Intent intent) {
-        ConnectivityManager cm=(ConnectivityManager)
+        ConnectivityManager cm = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         @SuppressWarnings("deprecation")
-                boolean isNetworkAvailable=cm.getBackgroundDataSetting()&&
-                cm.getActiveNetworkInfo()!=null;
-        if (!isNetworkAvailable)return;
+        boolean isNetworkAvailable = cm.getBackgroundDataSetting() &&
+                cm.getActiveNetworkInfo() != null;
+        if (!isNetworkAvailable) return;
 
-        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
-        String query=prefs.getString(FlickrFetchr.PREF_SEARCH_QUERY, null);
-        String lastResultId=prefs.getString(FlickrFetchr.PREF_LAST_RESULT_ID,null);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String query = prefs.getString(FlickrFetchr.PREF_SEARCH_QUERY, null);
+        String lastResultId = prefs.getString(FlickrFetchr.PREF_LAST_RESULT_ID, null);
 
-        ArrayList<GalleryItem>items;
-        if (query!=null){
-            items=new FlickrFetchr().search(query);
-        }else {
-            items=new FlickrFetchr().fetchItems();
+        ArrayList<GalleryItem> items;
+        if (query != null) {
+            items = new FlickrFetchr().search(query);
+        } else {
+            items = new FlickrFetchr().fetchItems();
         }
-        if (items.size()==0)
+        if (items.size() == 0)
             return;
-        String resultId=items.get(0).getId();
+        String resultId = items.get(0).getId();
 
-        if (!resultId.equals(lastResultId)){
-            Log.i(TAG,"Got a new result: "+resultId);
-        }else {
-            Log.i(TAG,"Got an old result: "+resultId);
+        if (!resultId.equals(lastResultId)) {
+            Log.i(TAG, "Got a new result: " + resultId);
         }
+
+        Resources r = getResources();
+        PendingIntent pi = PendingIntent
+                .getActivities(this, 0, new Intent[]{new Intent(this, PhotoGalleryActivity.class)}, 0);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setTicker(r.getString(R.string.new_picture_title))
+                .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                .setContentTitle(r.getString(R.string.new_picture_title))
+                .setContentTitle(r.getString(R.string.new_picture_text))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+
         prefs.edit()
                 .putString(FlickrFetchr.PREF_LAST_RESULT_ID,resultId)
                 .commit();
-
         Log.i(TAG, "Received an intent: " + intent);
     }
     public static void setServiceAlarm(Context context,boolean isOn){
