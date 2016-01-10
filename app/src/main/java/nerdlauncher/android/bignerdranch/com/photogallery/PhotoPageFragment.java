54192820ae2d6d14d1e1b59645de6fda0a5263ae
slad08 <slad_den@mail.ru>
2016-01-10 23:22:15 +0300

    WebView pg 516

--- app/src/main/java/nerdlauncher/android/bignerdranch/com/photogallery/PhotoPageFragment.java
+++ app/src/main/java/nerdlauncher/android/bignerdranch/com/photogallery/PhotoPageFragment.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Created by Denis on 10.01.2016.
@@ -27,12 +30,30 @@ public class PhotoPageFragment extends VisibleFragment {
                              Bundle savedInstanceState){
         View vv=inflater.inflate(R.layout.fragment_photo_page,parent,false);
 
+        final ProgressBar progressBar=(ProgressBar)vv.findViewById(R.id.progressBraMy);
+        progressBar.setMax(100);
+        final TextView titleTextView=(TextView)vv.findViewById(R.id.titleTextVIew);
+
         mWebView=(WebView)vv.findViewById(R.id.webView_my);
         mWebView.getSettings().setJavaScriptEnabled(true);
 
         mWebView.setWebViewClient(new WebViewClient() {
-            public boolean shouldOverrideUrlLoading(WebView view, String url){
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 return false;
+            }
+        });
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView webView, int progress) {
+                if (progress == 100) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(progress);
+                }
+            }
+            public void onReceivedTitle(WebView webView,String title){
+                titleTextView.setText(title);
             }
         });
         mWebView.loadUrl(mUrl);
